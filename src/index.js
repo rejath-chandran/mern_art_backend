a6e9cc3a484901772ece6ba0f2d2b1ec8d1efcc0
rejath-chandran <rajatchandran13@gmail.com>
2024-01-03 23:14:40 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const PORT= process.env.PORT
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.json({
+        name:"rejath"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log("server started on :",PORT)
+})
